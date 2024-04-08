033f610d356a7ace48405d55a25fe2815939265f
AiaPrinci <davidjimsheleishvili17@gmail.com>
2024-04-08 19:48:08 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+for(let i=5; i<15; i++){
+    console.log(i, 'Phantoms ar vgavar');
+}
+
+let Frank = 10;
+
+while(Frank < 20){
+    console.log(Frank, 'Frank ar gewyinos dzma');
+    Frank++;
+}
+
+// let Aia = 100;
+
+// do{
+//     console.log(Aia, 'Kutaisi Gulshi');
+//     Aia++;
+// }while(Aia > 90);
+
+let aia = 10;
+
+
+function typeAia(){
+    console.log('AIA')
+}
+
+if(aia > 100){
+    console.log('Kutaisi cudia');
+}else if(aia < 100){
+    console.log('Kutaisi N1');
+    typeAia();
+}
+
+let Kutaisi = 10;
+
+function increaseNumbers(test){
+    console.log(test);
+}
+
+if(Kutaisi > 5){
+    increaseNumbers('Kutaisi');
+}
+
+if(Kutaisi > 9){
+    increaseNumbers('West Side');
+}
